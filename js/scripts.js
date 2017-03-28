47b1f03f09848ede222c139252ab458e5fcbef04
Dominic Brown and Marko Ludlow <dominic@>
2017-03-28 09:37:45 -0700

    add js to remove As from sentence

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,30 @@ $(document).ready(function() {
 
     var startingSentence = $("#sent1").val();
 
+    var tmpArray = startingSentence.split("");
+    var noAsArray = [];
+
+    console.log("Type of: " , typeof tmpArray[2], tmpArray[2]);
+
+    for (i = 0; i <= (tmpArray.length - 1) ; i++) {
+
+      if (tmpArray[i] != ("a" || "A")) {
+        noAsArray.push(tmpArray[i]);
+      } else {
+        noAsArray.push("-")
+      }
+
+    }
+
+    console.log("no As: " , noAsArray);
+
+
+
+
     $("#output1").text(startingSentence);
-    $("#output2").text();
+    $("#output2").text(tmpArray);
+
+
   });
 
 });
