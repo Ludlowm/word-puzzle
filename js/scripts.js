5af01960e047f62e75375d1250433e4a19409402
Dominic Brown and Marko Ludlow <dominic@>
2017-03-28 10:22:20 -0700

    finish logic, all working

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,9 @@ $(document).ready(function() {
     var tmpArray = startingSentence.split("");
     var noAsArray = [];
 
-    console.log("Type of: " , typeof tmpArray[2], tmpArray[2]);
+    // console.log("Type of: " , typeof tmpArray[2], tmpArray[2]);
 
     for (i = 0; i <= (tmpArray.length - 1) ; i++) {
-
       if ( (tmpArray[i] === "a") || (tmpArray[i] === "A") )  {
         noAsArray.push("-");
       } else if ( (tmpArray[i] === "e") || (tmpArray[i] === "E") ) {
@@ -25,14 +24,12 @@ $(document).ready(function() {
       } else {
         noAsArray.push(tmpArray[i]);
       }
-
     }
 
-    console.log("no As: " , noAsArray);
-
+    noAsArray = noAsArray.join("");
 
     $("#output1").text(startingSentence);
-    $("#output2").text(tmpArray);
+    $("#output2").text(noAsArray);
 
 
   });
